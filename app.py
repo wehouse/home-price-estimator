e6a9dcb0bd3e29283200ae12e11a0856ef88834f
Daniel Yinanc <noemails@noemails.com>
2017-11-02 18:31:13 -0400

    First functional prediction on linear model

---
+++ app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 13:25:57 2017
+
+@author: dyinanc
+"""
+import pandas as pd
+import numpy as np
+
+
+from linear_model import KingCountyLinearModel
+dataset = pd.read_csv('kc_house_data.csv')
+
+kingC = KingCountyLinearModel()
+regressor,onehotencoder= kingC.create_regressor()
+print(regressor)
+
+zipcode=98002
+bedrooms=3
+bathrooms=2.5
+floors=2
+sqft_above=2000
+sqft_basement=800
+year_built=2010
+sqft_living=2100
+sqft_lot=2600
+
+zipcode_loc =list(onehotencoder.active_features_).index(98002)
+input_arr = np.zeros(len(list(onehotencoder.active_features_))-1)
+input_arr[zipcode_loc-1] = 1
+input_arr = np.append(input_arr, bedrooms)
+input_arr = np.append(input_arr, bathrooms)
+input_arr = np.append(input_arr, floors)
+input_arr = np.append(input_arr, sqft_above)
+input_arr = np.append(input_arr, sqft_basement)
+input_arr = np.append(input_arr, year_built)
+input_arr = np.append(input_arr, sqft_living)
+input_arr = np.append(input_arr, sqft_lot)
+
+regressor.predict([input_arr])
+
+#print(list(onehotencoder.active_features_).index(98002))
+# 1. zipcode - int (All zipcodes except 98001 will be encoded as param #1 ~ due to dummy variable trap)
+# 2. bedrooms - int 
+# 3. bathrooms - float
+# 4. floors - int
+# 5. sqft_above - int
+# 6. sqft_basement - int
+# 7. year_built - int
+# 8. zipcode - int
+# 9. sqft_living - int
+# 10. sqft_lot - int
---
+++ linear_model_exp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 14:42:06 2017
+
+@author: dyinanc
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('kc_house_data.csv')
+current=0
+selected_columns=[]
+for column in dataset.columns.values:
+    if(current in list(range(3,5)) +list(range(7,8))+ list(range(12,15)) + list(range(16,17)) +list(range(19,21))):
+        selected_columns.append(column)
+    current+=1
+X = dataset.iloc[:, list(range(3,5)) +list(range(7,8))+ list(range(12,15)) + list(range(16,17)) +list(range(19,21))].values
+y = dataset.iloc[:, 2].values
+
+# HotEncode Zipcodes
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+onehotencoder = OneHotEncoder(categorical_features = [6])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Model split into training and test
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Avoid dummy variable trap
+X = X[:,1:]
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+import statsmodels.formula.api as sm
+X_train = np.append(arr=np.ones((len(X_train),1)).astype(int),values=X_train,axis=1)
+X_opt = X_train[:,list(range(0,79))]
+regressor_OLS = sm.OLS(endog=y_train,exog=X_opt).fit()
+regressor_OLS.summary()
