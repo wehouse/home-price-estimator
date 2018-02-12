060f1e007af93c26e642a7c2e856b35a2cdc0adc
Daniel Yinanc <noemails@noemails.com>
2018-02-12 10:23:52 -0500

    CORS added via Falcon CORS

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import falcon
+from falcon_cors import CORS
+
 from multiple_linear_resource import MultipleLinearResource 
 
 
+public_cors = CORS(allow_all_origins=True)
 
 
-api = falcon.API()
+api = falcon.API(middleware=[public_cors.middleware])
 api.add_route('/multiple-linear', MultipleLinearResource())
