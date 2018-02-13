78375d558d7ffab86609e58e25f5c70f55e4e07a
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2018-02-12 20:42:18 -0500

    Fully resolved CORS problem

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from falcon_cors import CORS
 
 from multiple_linear_resource import MultipleLinearResource 
 
-
-public_cors = CORS(allow_all_origins=True, allow_all_headers=True)
+public_cors = CORS(allow_all_origins=True, allow_all_methods=True, allow_all_headers=True, allow_headers_list=['Content-Type'])
 
 
 api = falcon.API(middleware=[public_cors.middleware])
